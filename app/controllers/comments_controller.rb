2e1b860fcb7735505f0f4711a85ad26c239d8705
kunihitouehara <uekuni2000@gmail.com>
2021-10-05 14:59:30 +0900

    追加:devise日本語化+フラッシュメッセージ

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
+
 
   # GET /comments
   def index
