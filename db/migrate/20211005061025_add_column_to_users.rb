d27c4746f9bb2771bcf239830e7bbf9f0c9fbb7d
kunihitouehara <uekuni2000@gmail.com>
2021-10-05 15:14:53 +0900

    追加:Userテーブルにadmin追加

---
+++ db/migrate/20211005061025_add_column_to_users.rb
@@ -0,0 +1,8 @@
+class AddColumnToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :like_hack, :text
+    add_column :users, :icon, :string
+    add_column :users, :admin, :boolean, default: false
+  end
+end
