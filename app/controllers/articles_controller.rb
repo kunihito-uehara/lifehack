3c3641e1a20970375ec1ab44f54a1b2d6ae8ab8a
kunihitouehara <uekuni2000@gmail.com>
2021-10-06 18:52:23 +0900

    記事CURD調整中

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,45 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    article = Article.new(article_params)
+    if @article.save
+      redirect_to articles_path, notice: "記事「#{@article.title}」を登録しました！"
+    else
+      render :new 
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @article.update(article_params)
+      redirect_to articles_path, notice: "記事「#{@article.title}」を更新しました！"    
+    else
+      render :edit
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path, notice: "記事「#{@article.title}」を削除しました！"
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+
   root 'comments#index'
+  # root 'articles#top'
+  # resources :articles
+
+  devise_for :users
   resources :comments
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  end
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
