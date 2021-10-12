0215b4758372d14b4e3c742684ef5cd3e3009773
kunihitouehara <uekuni2000@gmail.com>
2021-10-12 11:33:24 +0900

    GEMキャリアウェーブ・ミニマジック導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
 class ApplicationController < ActionController::Base
   #deviseにおけるストロングパラメータ
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource) #deviseでログイン機能実装後に、ログイン後の遷移先を指定
     articles_path
   end
 
-  # protected
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :icon,:like_hack])
-  # end
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :icon,:like_hack])
+  end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :icon,:like_hack])
+  end
 end
